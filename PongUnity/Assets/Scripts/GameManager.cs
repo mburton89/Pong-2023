6166820a7ae0f588a393de36099ca4fbf6744d7f
WUStudent <sophbishopcross@gmail.com>
2023-09-06 16:32:40 -0500

    Updated GameManager & Placed TMP

--- PongUnity/Assets/Scripts/GameManager.cs
+++ PongUnity/Assets/Scripts/GameManager.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    
     public int mineral1Count;
     public int mineral2Count;
     public int mineral3Count;
 
+    public TextMeshProUGUI mineralCounter;
+
     public float gunCharge;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
