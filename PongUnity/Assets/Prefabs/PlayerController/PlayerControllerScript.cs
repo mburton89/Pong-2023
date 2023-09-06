0f4784ff6a46edfc728b5750afd5c7729d2b30b8
WUStudent <wustudent@webster.edu>
2023-09-06 16:25:25 -0500

    PlayerController
    stubbed out class

---
+++ PongUnity/Assets/Prefabs/PlayerController/PlayerControllerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    public float movementSpeed;
+    public GameObject miningLaser;
+    public float jumpHeight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Move()
+    {
+
+    }
+
+    void Jump()
+    {
+
+    }
+
+
+     
+}
