4d660c352cb4a313d70e706e6809e81ea129d912
WUStudent <sophbishopcross@gmail.com>
2023-09-06 16:22:02 -0500

    Created GameManager Class

---
+++ PongUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int mineral1Count;
+    public int mineral2Count;
+    public int mineral3Count;
+
+    public float gunCharge;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateMineralCountUI()
+    {
+
+    }
+
+    public void UpdateGunChargeUI()
+    {
+
+    }
+
+}
