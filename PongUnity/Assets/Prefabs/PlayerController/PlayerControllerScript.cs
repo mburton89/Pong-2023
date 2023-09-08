faa15db35bccd422c0031e421ca0be8fe3b9c4dc
WUStudent <wustudent@webster.edu>
2023-09-08 15:33:33 -0500

    Added movement and rotation
    from this series of tutorials https://www.youtube.com/watch?v=c-R8sB2zDLQ

--- PongUnity/Assets/Prefabs/PlayerController/PlayerControllerScript.cs
+++ PongUnity/Assets/Prefabs/PlayerController/PlayerControllerScript.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-    public float movementSpeed;
-    public GameObject miningLaser;
+    public float jogSpeed;
+    public float sprintSpeed;
     public float jumpHeight;
+    public float mouseSensitivity;
+
+    public GameObject miningLaser;
+    public Rigidbody rb;
+    public Transform camera;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivity);
     }
+
+    private void LateUpdate()
+    {
+        Vector3 e = camera.eulerAngles;
+        e.x -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        e.x = RestrictAngle(e.x, -85f, 85f);
+        camera.eulerAngles = e;
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 newVelocity = Vector3.up * rb.velocity.y;
+        float speed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : jogSpeed;
+        newVelocity.x = Input.GetAxis("Horizontal") * speed;
+        newVelocity.z = Input.GetAxis("Vertical") * speed;
+        rb.velocity = transform.TransformDirection(newVelocity);
+    }   
 
     void Move()
     {
@@ -30,6 +53,16 @@ public class PlayerControllerScript : MonoBehaviour
     void Jump()
     {
 
+    }
+
+    public static float RestrictAngle(float angle, float angleMin, float angleMax)
+    {
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -180)
+            angle += 360;
+
+        return angle;
     }
 
 
